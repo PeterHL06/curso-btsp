0bdb03761c57cef23b5715475ad95cd841662b89
PeterHL06 <hurtado.lopez.pedro.6@gmail.com>
2025-07-21 10:32:55 -0600

    Creación docket-compose

---
+++ BsDa/Postgresql/Bases de Datos/app_comida.sql
@@ -0,0 +1,11 @@
+CREATE TABLE restaurante{
+    nombre VARCHAR(50),
+    direccion VARCHAR(60),
+    tipo_comida VARCHAR(50)
+};
+
+CREATE TABLE repartidor{
+    nombre_completo VARCHAR(60),
+    num_id INTEGER,
+    vehiculo VARCHAR(60)
+};
---
+++ BsDa/Postgresql/Bases de Datos/biblioteca.sql
@@ -0,0 +1,41 @@
+CREATE TABLE libro{
+    titulo VARCHAR(60),
+    autor VARCHAR(50),
+    paginas INT
+};
+
+CREATE TABLE alumno{
+    nombre_completo VARCHAR(50),
+    matricula CHAR(10),
+    grado_escolar INT
+};
+
+
+
+/*
+    Enteros:
+    int / integer
+    bigint
+    smallint
+    tinyint
+
+    flotantes:
+    float
+    double
+    numeric
+    real
+
+    "cadenas"
+    char(n) longitud fija
+    varchar(n) longitud variable
+    text cadena ilimitada.
+
+    Lógicos
+    boolean  TRUE, FALSE, NULL
+
+    Fechas
+    date
+    time
+    timestamp
+    timestamptz
+*/
