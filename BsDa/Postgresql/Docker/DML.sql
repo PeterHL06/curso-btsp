09a8fe6df0a95f8ea3764a35406080d3c838e7dd
PeterHL06 <hurtado.lopez.pedro.6@gmail.com>
2025-07-28 07:44:29 -0600

    Comienzo con SQL

---
+++ BsDa/Postgresql/Docker/DML.sql
@@ -0,0 +1,18 @@
+/*
+    INSERT --> Insertar elementos en la base de datos /tabla
+    UPDATE --> Actualizar los elementos dentro de una tabla
+    DELETE --> Eliminar los elementos dentro de una tabla
+    Modificaciones permanentes !!
+
+*/
+
+-- INSERT INTO tabla() values ();
+
+INSERT alumnos(
+    matricula, nombre_completo, edad, fecha_alta, sancionado, carrera
+)
+values(
+    'A070', 'Cristina Colín Ayala', 25, '2025-07-24', TRUE, 'Matemáticas',
+    'A060','Pedro Hurtado López',40,'2025-07-24', TRUE, 'Electrónica'
+)
+
