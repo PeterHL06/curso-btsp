cbeecedc8eb8afb72cc88d10c7229124c54e6630
PeterHL06 <hurtado.lopez.pedro.6@gmail.com>
2025-07-28 07:45:18 -0600

    Comienzo con SQL_2

--- BsDa/Postgresql/Docker/DML.sql
+++ BsDa/Postgresql/Docker/DML.sql
@@ -10,9 +10,9 @@
 
 INSERT alumnos(
     matricula, nombre_completo, edad, fecha_alta, sancionado, carrera
-)
+);
 values(
     'A070', 'Cristina Colín Ayala', 25, '2025-07-24', TRUE, 'Matemáticas',
     'A060','Pedro Hurtado López',40,'2025-07-24', TRUE, 'Electrónica'
-)
+);
 
