795ce149b8f330a4198d9d4347c69702df049af4
Hurtado Pedro <pedro.hurtado@posadas.com>
2025-08-26 12:55:29 -0600

    Uso Polimorfismo y clases padre e hijas

--- Java/clase8/sistemaNotificaciones.java
+++ Java/clase8/sistemaNotificaciones.java
@@ -1,4 +1,7 @@
-abstract class Notifificacion {
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Notificacion {
 
     protected String destinatario;
     protected String mensaje;
@@ -9,7 +12,7 @@ abstract class Notifificacion {
         this.mensaje=mensaje;
     }
 
-    //Metodo
+    //Metodo abstracto
     public abstract void envio();
 
 
@@ -19,15 +22,58 @@ class Email extends Notificacion{
     public Email(String destinatario, String mensaje){
         super(destinatario, mensaje);
     }
-    @Override
+
+    @Override //Sobreescritura
     public void envio(){
         System.out.println("Enviando Email a: "+destinatario);
         System.out.println("Mensaje: "+mensaje);
     }
 }
 
+class SMS extends Notificacion{
+    public SMS(String destinatario, String mensaje){
+
+        super(destinatario, mensaje);
+
+    }
+
+    @Override
+    public void envio(){
+        System.out.println("Enviando SMS a: "+destinatario);
+        System.out.println("Mensaje: "+mensaje);
+    }
+}
+
+class Push extends Notificacion{
+    public Push(String destinatario, String mensaje){
+
+        super(destinatario, mensaje);
+
+    }
+
+    @Override
+    public void envio(){
+        System.out.println("Enviando notificación Push a: "+destinatario);
+        System.out.println("Mensaje: "+mensaje);
+    }
+}
+
 public class sistemaNotificaciones {
     public static void main(String[] args) {
+
+        List<Notificacion> listaNotificaciones = new ArrayList<>(); //Objeto a partir de clase padre
+
+        //Añadir elementos a la lista usando las clases hijas
+        System.out.print("\n");
+        listaNotificaciones.add(new Email("pedro.hurtado@gmail.com", "Su pedido está en camino"));
+        listaNotificaciones.add(new SMS("443-184-97-44", "Tu código de seguridad es: 281084"));
+        listaNotificaciones.add(new Push("usuario_PHL06", "Hay nuevo articulos que te pueden gustar"));
         
+        //Iterar la lista
+        for (Notificacion noti: listaNotificaciones){
+            noti.envio();
+            System.out.println("------");
+            System.out.print("\n");            
+        }
     }
 }
---
+++ Java/clase9/procesadorDocumentos.java
@@ -0,0 +1,71 @@
+package Java.clase9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Documento {
+
+    protected String nombreArchivo;
+
+    //Constructor
+    public Documento(String nombreArchivo){
+        this.nombreArchivo=nombreArchivo;
+
+    }
+
+    //Metodo abstracto
+    public abstract void procesar();
+
+
+}
+
+class PDF extends Documento{
+    public PDF(String nombreArchivo){
+        super(nombreArchivo);
+    }
+
+    @Override //Sobreescritura
+    public void procesar(){
+        System.out.println("Procesando documento PDF... "+nombreArchivo);    }
+}
+
+class Word extends Documento{
+    public Word(String nombreArchivo){
+        super(nombreArchivo);
+    }
+
+    @Override //Sobreescritura
+    public void procesar(){
+        System.out.println("Procesando documento Word... "+nombreArchivo);    }
+}
+
+class Imagen extends Documento{
+    public Imagen(String nombreArchivo){
+        super(nombreArchivo);
+    }
+
+    @Override //Sobreescritura
+    public void procesar(){
+        System.out.println("Procesando archivo de imagen... "+nombreArchivo);    }
+}
+
+
+public class procesadorDocumentos {
+
+    public static void main(String[] args) {
+        List<Documento> listaDocumentos = new ArrayList<>(); //Objeto a partir de clase padre
+
+        //Añadir elementos a la lista usando las clases hijas
+        System.out.print("\n");
+        listaDocumentos.add(new PDF("Estado de cuenta"));
+        listaDocumentos.add(new Word("Reporte semanal"));
+        listaDocumentos.add(new Imagen("Logo de la empresa"));
+        
+        //Iterar la lista
+        for (Documento noti: listaDocumentos){
+            noti.procesar();
+            System.out.println("------");
+            System.out.print("\n"); 
+    }
+}    
+}
